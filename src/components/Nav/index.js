89e7b8e72659883b59203815edc199ec77f15ab8
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-12-19 09:31:44 -0800

    add  about me component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <nav>
+      <div className="nav-logo">Logo</div>
+      <div className="nav-item">About Me</div>
+      <div className="nav-item">Portafolio</div>
+      <div className="nav-item">Contact Me</div>
+      <div className="nav-item">Resume</div>
+    </nav>
+  );
+};
+
+export default Nav;
