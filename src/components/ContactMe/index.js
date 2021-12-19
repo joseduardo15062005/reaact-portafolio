ed0b98245aad4c31885a5038b2e91226aa8c3768
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-12-19 13:24:31 -0800

    add form functionality onBlur and Submit

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -1,26 +1,80 @@
+import { useState } from "react";
 import Image from "../../assets/images/contact-me.svg";
 
 const ContactMe = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+  const [formMessage, setFormMessage] = useState("Please Fill Out Form");
+
+  const handleBlur = (e) => {
+    if (!e.target.value.length) {
+      setFormMessage(`${e.target.name} is required`);
+    } else {
+      setFormMessage("");
+    }
+
+    //Validate if email is valid
+    if (e.target.name === "email") {
+      if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e.target.value)) {
+        setFormMessage("Please enter a valid email");
+      } else {
+        setFormMessage("");
+      }
+    }
+
+    if (!formMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!formMessage) {
+      console.log(formState);
+    } else {
+      console.log(formMessage);
+    }
+  };
+
   return (
     <section>
       <div className="page-body">
         <img src={Image} alt="Contact Me" className="img-body" />
         <h1>Contact Me</h1>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <div className="form-input">
-            <input type="text" placeholder="Name" />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onBlur={handleBlur}
+            />
           </div>
           <div className="form-input">
-            <input type="email" placeholder="Email" />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onBlur={handleBlur}
+            />
           </div>
           <div className="form-input">
-            <textarea placeholder="Message" />
+            <textarea
+              placeholder="Message"
+              name="message"
+              onBlur={handleBlur}
+            />
           </div>
           <div className="form-button">
             <button type="submit" className="button">
               Send
             </button>
           </div>
+          <div className="form-message">{formMessage}</div>
         </form>
       </div>
     </section>
