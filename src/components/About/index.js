479a361eaf2efc293b13670709006dd3fe13ac07
joseduardo15062005 <joseduardo15062005@gmail.com>
2022-01-16 12:14:34 -0800

    fix about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 import ImageAboutMe from "../../assets/images/about-me.svg";
+import ImageMe from "../../assets/images/me.png";
 
 const About = () => {
   return (
@@ -6,47 +7,14 @@ const About = () => {
       <div className="page-body">
         <img src={ImageAboutMe} alt="About Me" className="img-body" />
         <h1>Who am I?</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-          ultricies sem eget accumsan feugiat. Curabitur ut accumsan augue.
-          Pellentesque bibendum arcu a pretium rhoncus. Aenean ut eros
-          ullamcorper, interdum tellus vitae, hendrerit nisl. Donec tincidunt
-          nisi leo, condimentum sodales augue consequat a. Aliquam a congue
-          risus. Quisque bibendum congue vulputate. Integer in odio malesuada,
-          aliquam justo non, sollicitudin mi. Ut vestibulum mattis tortor vel
-          vehicula. Maecenas tristique justo sapien, nec laoreet dolor rutrum
-          sed. Vivamus maximus viverra rhoncus. Quisque nibh magna, bibendum
-          tempus lectus id, finibus placerat augue. Mauris venenatis luctus nunc
-          in aliquet. Nulla at ornare ligula. Duis efficitur lectus eget ipsum
-          efficitur, at pharetra tortor volutpat. Aenean ac nisi risus. Ut
-          dapibus urna arcu. Ut consequat tristique nulla ut faucibus. Quisque
-          rhoncus non lorem non efficitur. Donec lacus enim, condimentum at
-          tempor eu, malesuada vel mi. Etiam orci neque, commodo et arcu eu,
-          mattis sodales justo. Mauris vel velit est. Pellentesque eleifend diam
-          eu felis consequat, a volutpat ante varius. Donec ex ex, accumsan sed
-          velit ut, vehicula condimentum mi. Praesent molestie dui vitae mauris
-          scelerisque, ac mollis lacus ullamcorper. Mauris et interdum risus, at
-          malesuada purus. Praesent dictum vel ipsum sed consequat. Nulla
-          facilisi. In semper velit at magna sagittis pretium. Aliquam mollis
-          quam id sodales suscipit. Fusce eget facilisis ligula. Pellentesque
-          non vulputate velit. Pellentesque vel accumsan ipsum. Etiam in dapibus
-          mauris, vel aliquet turpis. Cras congue tincidunt felis vitae aliquet.
-          Nulla vulputate felis vestibulum lacinia dictum. Nunc nisi lacus,
-          eleifend eget felis lacinia, facilisis pulvinar nibh. Nam ut porttitor
-          felis. Sed rutrum nisi quis vehicula consequat. Nunc vestibulum, ante
-          semper tincidunt hendrerit, ligula elit volutpat elit, sed euismod
-          justo libero vitae enim. Quisque feugiat ac sapien efficitur
-          dignissim. Donec eget erat et nunc viverra fringilla. In sollicitudin
-          a nisi dictum imperdiet. Ut purus mi, mollis vel sem non, tempor
-          tempor eros. Curabitur eu mauris iaculis, posuere justo eget, iaculis
-          lacus. Integer et dapibus massa, eget rhoncus lorem. Ut laoreet ornare
-          posuere. Nulla consequat nunc vel diam maximus elementum. Sed eu
-          turpis hendrerit nisi posuere accumsan sed et massa. Duis ac cursus
-          ligula. Maecenas mauris odio, ultrices sed nisi nec, hendrerit
-          facilisis erat. Duis tempor vehicula ex, vulputate consequat felis.
-          Vivamus eget augue neque. Sed dolor erat, facilisis eu ultricies ut,
-          condimentum ut tortor. Donec ut pulvinar magna.
-        </p>
+        <div className="flex-container">
+          <img src={ImageMe} alt="Jose Velasquez" className="img-me"></img>
+
+          <p>
+            Hello I Jose, I'm a Front-end developer, I have a passion for
+            programming and I'm always looking for new challenges.
+          </p>
+        </div>
       </div>
     </section>
   );
